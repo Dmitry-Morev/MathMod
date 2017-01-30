29ddcd8818909561a9c8f4a7b2f497433e5c1bbe
Dmitry V. Morev <dvmorev@hotmail.com>
2017-01-31 01:19:56 +0300

    Проба семинар 1

---
+++ test_seminar-1.R
@@ -0,0 +1,21 @@
+plot(density(rnorm(100)),col="red")
+x=5
+x<-5
+assign("x",5)
+x
+x<-c(1,3,6,7)
+x
+char<-c("8","edge","turn8k")
+char
+logic<-c(TRUE,FALSE,TRUE,FALSE)
+logic
+dynam<-c(8,"dds", TRUE)
+dynam<-c(1,0,1,1,TRUE)
+dynam
+log(x,10)
+x=10/(5-5)
+x
+x<-c(-10,2,78,34,-98,23,0,-8)
+order(x)
+sort(x)
+sort(x) == x[order(x)]
